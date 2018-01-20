d8bf9ce8628c3da5ffba4406fc71c8cb301325a8
Rishabh Gupta <rishabh.gupta.1606@gmail.com>
2018-01-20 18:06:24 -0500

    data conversion scripts

--- apollo.py
+++ apollo.py
@@ -8,10 +8,26 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.autograd import Variable
 import torchvision.models as models
+import torchvision.datasets as datasets
 
-resnet18 = models.resnet18(pretrained=True)
+net = models.resnet18(pretrained=True)
 
 transform = transforms.Compose([transforms.ToTensor(),
                                 transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                                      std=[0.229, 0.224, 0.225])])
-trainset = torchvision.datasets.
+trainset = datasets.ImageFolder(root=traindir, transform=transform)
+trainloader = torch.utils.data.DataLoader(
+    trainset, batch_size=4, shuffle=True, num_workers=0)
+
+# display image
+def imshow(img):
+    img = img * 0.229 + 0.485  # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+
+ouputs = net(Variable(images.cuda()))
+_, predicted = torch.max(outputs.data, 1)
